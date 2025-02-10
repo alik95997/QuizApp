4ca56e468978b3ac861b4c85b92898b0a5b8ce40
Ali Khan <88495045+alik95997@users.noreply.github.com>
2025-02-10 23:58:38 +0500

    Update login.js

--- auth/login/login.js
+++ auth/login/login.js
@@ -17,7 +17,7 @@ const authenticationCheck = () => {
     let user = localStorage.getItem("user");
     user = JSON.parse(user);
     if (user.type === "user") {
-      window.location.replace("user/dashboard/user-dashboard.html");
+      window.location.replace("/quizApp/user/userdashboard/userdashboard.html");
     } else if (user.type === "admin") {
       window.location.replace("admin/dashboard/dashboard.html");
     }
